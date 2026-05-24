259638996a646bff8f13c873037666466a54d9c0
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-05-23 21:15:19 -0300

    fix: inicializar contador com 0 em exercicio2

--- funcoes/exercicios/exercicio2/exercicio2.c
+++ funcoes/exercicios/exercicio2/exercicio2.c
@@ -1,31 +1,29 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int vogais(char letra){
+int vogais(char letra)
+{
 
     return (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
-        letra == 'A' || letra== 'E' || letra == 'I' || letra == 'O' || letra == 'U');
-
-    
+            letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U');
 }
 
-int main() {
-    char palavra [100];
+int main()
+{
+    char palavra[100];
     int contador;
 
     printf("Digite uma palavra: ");
     scanf("%s", palavra);
 
-    for(int i; palavra[i] != '\0'; i++){
-        if(vogais(palavra[i])){
+    for (int i = 0; palavra[i] != '\0'; i++)
+    {
+        if (vogais(palavra[i]))
+        {
             contador++;
         }
     }
 
-
     printf("A palavra %s tem %d vogais", palavra, contador);
-
-
-
 
     return 0;
 }
